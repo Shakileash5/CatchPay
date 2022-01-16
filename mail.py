cab5806fcce3bf17081dd6c75b1001df9668b7fe
Shakileash C <shakileash2000@gmail.com>
2022-01-16 22:14:05 +0530

    update mail.py

--- mail.py
+++ mail.py
@@ -2,9 +2,9 @@ import smtplib, ssl
 
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = "shakileash2000@gmail.com"  # Enter your address
-receiver_email = "sirenatechnology@gmail.com"  # Enter receiver address
-password = "vvnurinqqtagonij"
+sender_email = ""  # Enter your address
+receiver_email = ""  # Enter receiver address
+password = ""
 messageConfirm = """\
 Subject: Confirmation mail
 
@@ -24,5 +24,3 @@ def send_mail(reciever_mail,msg,flag):
     with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, reciever_mail, message)
-
-#send_mail("atshaya1699@gmail.com","BTC")
